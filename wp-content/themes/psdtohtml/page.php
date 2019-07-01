f67ea1dfcdf4a2381ecbfb363e7adc518a489b2b
Julia Ptitsyna <julia.ptitsyna@gfe-media.de>
2019-07-01 17:16:58 +0300

    Full responsive Contact Us Page

--- wp-content/themes/psdtohtml/page.php
+++ wp-content/themes/psdtohtml/page.php
@@ -5,10 +5,10 @@
 ?>
 
 <?php get_header(); ?>
-    <div class="main-heading">
-        <h1><?php the_title(); ?></h1>
-    </div>
     <section role="main">
+        <div class="main-heading">
+            <h1><?php the_title(); ?></h1>
+        </div>
         <div id="main">
             <?php if (have_posts()): while (have_posts()): the_post(); ?>
                 <?php the_content(); ?>
