084eaaf982b68ec02b494e50fb9d7b4394532e49
Julia Ptitsyna <julia.ptitsyna@gfe-media.de>
2019-07-01 13:33:09 +0300

    Widht 980 fix, responsive footer, category page, refactoring

--- wp-content/themes/psdtohtml/header.php
+++ wp-content/themes/psdtohtml/header.php
@@ -3,6 +3,7 @@
 <head id="head">
     <meta http-equiv="Content-type" content="text/html; charset=<?php bloginfo('charset'); ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=Edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <title><?php wp_title('«', true, 'right'); ?><?php bloginfo('name'); ?></title>
     <?php wp_head(); ?>
