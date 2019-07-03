cd551f8277a81ef7fd65267e60843ff65eec3474
Julia Ptitsyna <julia.ptitsyna@gfe-media.de>
2019-07-03 16:00:32 +0300

    Single Post template

--- wp-content/themes/psdtohtml/single.php
+++ wp-content/themes/psdtohtml/single.php
@@ -1,12 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/#single-post
- *
- * @package WordPress
- * @subpackage Twenty_Nineteen
- * @since 1.0.0
+ * Template Name: Single Post
  */
 ?>
 
