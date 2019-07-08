21d3a57c467977c2751acb57cfccd090eb639869
Julia Ptitsyna <julia.ptitsyna@gfe-media.de>
2019-07-08 18:16:45 +0300

    header. contacts fix

--- wp-content/themes/psdtohtml/functions.php
+++ wp-content/themes/psdtohtml/functions.php
@@ -29,7 +29,7 @@ function my_wp_nav_menu_items($items, $args)
 {
     $menu = wp_get_nav_menu_object($args->menu);
     $phone = get_field('phone', $menu);
-    $html = '<li id="menu-item-phone" class="menu-item">' . $phone . '</li>';
+    $html = '<li id="menu-item-phone" class="menu-item"><a>' . $phone . '<a></li>';
     $items = $html . $items;
 
     return $items;
